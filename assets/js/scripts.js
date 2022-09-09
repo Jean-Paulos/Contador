ecce1c2d3fcf2b0646eaad292ee7cbf341412e47
Jean-Paulos <110000423+Jean-Paulos@users.noreply.github.com>
2022-09-09 17:09:05 -0300

    Update scripts.js
    alteração nos if

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,31 +5,18 @@ var subtrair = document.getElementById('subtract');
 var soma = document.getElementById('sum'); 
 
 soma.addEventListener('click', function(){
-    if(currentNumber <= 9){
+    if(currentNumber < 10){
         currentNumber++;
         currentNumberWrapper.innerHTML = currentNumber;
         document.getElementById('currentNumber').style.color = "white";
-    }else {
-        return;
     }
 });
 
 subtract.addEventListener('click', function(){
-    if(currentNumber >= 0) {
+    if(currentNumber > 0) {
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
-    }else if(currentNumber <= 0){
-        document.getElementById('currentNumber').style.color = "red";
     }else {
-        return;
+        document.getElementById('currentNumber').style.color = "red";
     }
 });
-
-
-
-
-
-
-/* tente implementar os eventos usando o metodo addeventlistener
-crie condicionais que desabilitam o botão de incrementar ou decrementar quando count chegar a um determinado valor (ex 0 <= count > 10)
-muda a cor do texto em current_number para vermelho quando o numero for negativo */
